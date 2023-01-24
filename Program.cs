3101b8147ed524ccf2f0e6d9a22451a988d65e9d
Karthika <223090003@ge.com>
2023-01-24 17:36:54 +0530

    first commit for new UC

--- Program.cs
+++ Program.cs
@@ -6,19 +6,30 @@ namespace Address_book
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to address book management");          
-            addressBook adrs_book = new addressBook();
+            Console.WriteLine("Welcome to address system management");          
+            addressBook adrs_book_1 = new addressBook();
 
-            adrs_book.addContact();
+            adrs_book_1.addContact();
             Console.WriteLine("-----------------------------");
-            adrs_book.addContact();
-            adrs_book.displayAddressBook();
-           
-            adrs_book.editContact();
-            adrs_book.displayAddressBook();
+            adrs_book_1.addContact();
+            adrs_book_1.displayAddressBook();
 
-            adrs_book.deleteContact();
-            adrs_book.displayAddressBook();
+            addressBook adrs_book_2 = new addressBook();
+
+            adrs_book_2.addContact();
+            Console.WriteLine("-----------------------------");
+            adrs_book_2.addContact();
+            adrs_book_2.displayAddressBook();
+
+            addressSystem adrs_system = new addressSystem();
+            adrs_system.addAddressBook("address_book1", adrs_book_1);
+            adrs_system.addAddressBook("address_book2", adrs_book_2);
+
+            adrs_system.displayAddressSystem();
+
+            adrs_system.searchPerson("chennai");
+            adrs_system.countPerson("chennai", null);
+            adrs_system.countPerson("", "tn");
 
         }
     }
--- addressBook.cs
+++ addressBook.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
 
 namespace Address_book
 {
@@ -11,9 +14,8 @@ namespace Address_book
         public string state;
         public int zip;
         public long phone_num;
-        public Contact(string f_name, string l_name, string email, string address, string city, string state, int zip, long phone_num)
+        public Contact(string l_name, string email, string address, string city, string state, int zip, long phone_num)
         {
-            this.f_name = f_name;
             this.l_name = l_name;   
             this.email = email;
             this.address = address;
@@ -25,7 +27,7 @@ namespace Address_book
     }
     public class addressBook
     {
-        public List<Contact> address_book = new List<Contact>();
+        public IDictionary<string, Contact> address_book = new Dictionary<string, Contact>();
         public void addContact()
         {
             Console.Write("Enter first name: ");
@@ -45,10 +47,27 @@ namespace Address_book
             Console.Write("Enter phone number: ");
             long phone_num = Convert.ToInt64(Console.ReadLine());
 
-            Contact new_contact = new Contact(f_name, l_name, email, address, city, state, zip, phone_num);
-
-            this.address_book.Add(new_contact);
+            if (checkContactExist(f_name) == false)
+            {
+                Contact new_contact = new Contact(l_name, email, address, city, state, zip, phone_num);
+                this.address_book.Add(f_name, new_contact);
+            }
+            else
+            {
+                Console.WriteLine("Person already exists");
+            }
+            
         }
+
+        bool checkContactExist(string f_name)
+        {  
+            if(this.address_book.ContainsKey(f_name))
+            {
+                return true;
+            }
+            return false;
+        }
+
 
         public void editContact()
         {
@@ -60,47 +79,43 @@ namespace Address_book
             }
             else
             {
-                foreach(Contact person in this.address_book)
+                foreach(var person in this.address_book)
                 {
-                    if (person.f_name== f_name)
+                    if (person.Key == f_name)
                     {
                         Console.WriteLine("enter num option to edit:");
-                        Console.WriteLine("1:f_name, 2:l_name, 3:email, 4:address, 5:city, 6:state, 7:zip, 8:phone_num");
+                        Console.WriteLine("2:l_name, 3:email, 4:address, 5:city, 6:state, 7:zip, 8:phone_num");
                         int edit_option = Convert.ToInt32(Console.ReadLine());
 
                         switch(edit_option)
                         {
-                            case 1:
-                                Console.Write("Enter new f_name: ");
-                                person.f_name = Console.ReadLine();
-                                break;
                             case 2:
                                 Console.Write("Enter new l_name: ");
-                                person.l_name = Console.ReadLine();
+                                person.Value.l_name = Console.ReadLine();
                                 break;
                             case 3:
                                 Console.Write("Enter new email: ");
-                                person.email = Console.ReadLine();
+                                person.Value.email = Console.ReadLine();
                                 break;
                             case 4:
                                 Console.Write("Enter new address: ");
-                                person.address = Console.ReadLine();
+                                person.Value.address = Console.ReadLine();
                                 break;
                             case 5:
                                 Console.Write("Enter new city: ");
-                                person.city = Console.ReadLine();
+                                person.Value.city = Console.ReadLine();
                                 break;
                             case 6:
                                 Console.Write("Enter new state: ");
-                                person.state = Console.ReadLine();
+                                person.Value.state = Console.ReadLine();
                                 break;
                             case 7:
                                 Console.Write("Enter new zip: ");
-                                person.zip = Convert.ToInt32(Console.ReadLine());
+                                person.Value.zip = Convert.ToInt32(Console.ReadLine());
                                 break;
                             case 8:
                                 Console.Write("Enter new phone_num: ");
-                                person.phone_num = Convert.ToInt64(Console.ReadLine());
+                                person.Value.phone_num = Convert.ToInt64(Console.ReadLine());
                                 break;
                             default:
                                 break;
@@ -120,11 +135,11 @@ namespace Address_book
             }
             else
             {
-                foreach(Contact person in this.address_book)
+                foreach(var person in this.address_book)
                 {
-                    if (person.f_name == f_name)
+                    if (person.Key == f_name)
                     {
-                        this.address_book.Remove(person);
+                        this.address_book.Remove(person.Key);
                         break;
                     }
                 }
@@ -133,19 +148,19 @@ namespace Address_book
 
         public void displayAddressBook()
         {
-            Console.WriteLine("Address book: ")
+            Console.WriteLine("Address book: ");
             int i = 1;
-            foreach(Contact person in this.address_book)
+            foreach(var person in this.address_book)
             {
                 Console.WriteLine("Person " + i);
-                Console.WriteLine("     f_name: " + person.f_name);
-                Console.WriteLine("     l_name :" + person.l_name);
-                Console.WriteLine("     email :" + person.email);
-                Console.WriteLine("     address :" + person.address);
-                Console.WriteLine("     city :" + person.city);
-                Console.WriteLine("     state :" + person.state);
-                Console.WriteLine("     zip :" + person.zip);
-                Console.WriteLine("     phone_num :" + person.phone_num);
+                Console.WriteLine("     f_name: " + person.Key);
+                Console.WriteLine("     l_name :" + person.Value.l_name);
+                Console.WriteLine("     email :" + person.Value.email);
+                Console.WriteLine("     address :" + person.Value.address);
+                Console.WriteLine("     city :" + person.Value.city);
+                Console.WriteLine("     state :" + person.Value.state);
+                Console.WriteLine("     zip :" + person.Value.zip);
+                Console.WriteLine("     phone_num :" + person.Value.phone_num);
 
                 Console.WriteLine("-----------------------------");
                 i++;
@@ -153,5 +168,84 @@ namespace Address_book
             }
         }
 
+    }
+
+    public class addressSystem
+    {
+        public IDictionary<string, addressBook> address_system_dict = new Dictionary<string, addressBook>();
+
+        public void addAddressBook(string addrss_book_name, addressBook addrss_book)
+        {
+            this.address_system_dict.Add(addrss_book_name, addrss_book);
+        }
+
+        public void searchPerson(string city_state)
+        {
+            Console.WriteLine("persons in given state/city " + city_state + " is: ");
+            foreach(var adrs_book in this.address_system_dict)
+            {
+                foreach (var person in adrs_book.Value.address_book)
+                {
+                    if (person.Value.state == city_state || person.Value.city == city_state)
+                    {
+                        Console.WriteLine("Person: " + person.Key);
+                    }
+                }
+            }
+            
+        }
+
+        public void countPerson(string city, string state)
+        {
+            if (state != null)
+            {
+                int cnt_state = 0;
+                
+                foreach(var adrs_book in this.address_system_dict)
+                {
+                    cnt_state += adrs_book.Value.address_book.Values.Count(x => x.state == state);
+                }
+                
+                Console.WriteLine("Number of persons in given state: "+ state+" is: " + cnt_state);
+            }
+            if (city != null)
+            {
+                int cnt_city = 0;
+                foreach(var adrs_book in this.address_system_dict)
+                {
+                    cnt_city += adrs_book.Value.address_book.Values.Count(x => x.city == city);
+                }
+                
+                Console.WriteLine("Number of persons in given state: " + city + " is: " + + cnt_city);
+            }
+
+        }
+
+        public void displayAddressSystem()
+        {
+            foreach(var adrs_book in this.address_system_dict)
+            {
+                Console.WriteLine("Address book name: " + adrs_book.Key);
+                Console.WriteLine("Address book: ");
+                int i = 1;
+                foreach (var person in adrs_book.Value.address_book)
+                {
+                    Console.WriteLine("Person " + i);
+                    Console.WriteLine("     f_name: " + person.Key);
+                    Console.WriteLine("     l_name :" + person.Value.l_name);
+                    Console.WriteLine("     email :" + person.Value.email);
+                    Console.WriteLine("     address :" + person.Value.address);
+                    Console.WriteLine("     city :" + person.Value.city);
+                    Console.WriteLine("     state :" + person.Value.state);
+                    Console.WriteLine("     zip :" + person.Value.zip);
+                    Console.WriteLine("     phone_num :" + person.Value.phone_num);
+
+                    Console.WriteLine("-----------------------------");
+                    i++;
+
+                }
+
+            }
+        }
     }
 }
